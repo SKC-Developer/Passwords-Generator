f8e0a87d281c1d65d55560f95ce8f97b1e45d9b8
Shachar Kraus <48233379+SKC-Developer@users.noreply.github.com>
2019-06-29 08:32:05 +0300

    Add files via upload

---
+++ PassGen_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef PASSGEN_PRIVATE_H
+#define PASSGEN_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"SKC"
+#define FILE_VERSION	"1.0.0.0"
+#define FILE_DESCRIPTION	"A simple password lists generator."
+#define INTERNAL_NAME	"PassGen"
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"PassGen.exe"
+#define PRODUCT_NAME	"PassGen"
+#define PRODUCT_VERSION	"1.0.0.0"
+
+#endif /*PASSGEN_PRIVATE_H*/
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef unsigned __int128 u128;
 
@@ -11,42 +12,31 @@ u128 p(u128 x, u128 y)
 	return res;
 }
 
-void PrintBase(u128 num, int len, unsigned char base, FILE*file)
+void PassGen(u128 num, unsigned char base, unsigned char len,const char*letters,FILE*file)
 {
-	if (base > 62)return;
-	char c;
-	u128 exp;
-	for (u128 l = 0; l < len; l++)
+	for (int i = 0; i < len; i++)
 	{
-		exp = p(base, len - l - 1);
-		c = num / exp;
-
-		if (c >= 10 && c <= 35)
-			fputc(c - 10 + 'A', file);
-		else if (c >= 36 && c <= 62)
-			fputc(c - 36 + 'a', file);
-		else
-			fputc(c + '0', file);
-		num = num % exp;
+		fputc(letters[num % base],file);
+		num /= base;
 	}
-	fputc('\n', file);
+	fputc('\n',file);
 }
 
-void create_numbers(int len, unsigned char letters, FILE*file,double file_size, bool progress)
+void create_numbers(int len, char*letters, FILE*file,double file_size, bool progress)
 {
-	u128 max = p(letters, len);
+	size_t last_size=0,base=strlen(letters);
+	u128 max = p(base, len);
 	int precent = -1;
-	size_t last_size=0;
 	for (u128 x = 0; x < max; x++)
 	{
-		PrintBase(x, len, letters, file);
-		if (progress && ( precent != (int)((double)x * 100 / max) || (ftell(file)-last_size)/(1024*1024) ))
+		PassGen(x,base,len,letters,file);
+		if (progress && ( precent != (int)((double)x * 100 / max) || (_ftelli64(file)-last_size)/(1024*1024) ))
 		{
 			precent = (int)((double)x * 100 / max);
-			last_size=ftell(file);
-			fseek(file,0,SEEK_END);
-			printf("\rProgress: %d%% or %.3fGB of %.3fGB", precent,(double)ftell(file)/(1024*1024*1024),file_size);
-			fseek(file,last_size,SEEK_SET);
+			last_size=_ftelli64(file);
+			_fseeki64(file,0,SEEK_END);
+			printf("\rProgress: %d%% or %.3fGB of %.3fGB", precent,(double)_ftelli64(file)/(1024*1024*1024),file_size);
+			_fseeki64(file,last_size,SEEK_SET);
 		}
 	}
 	putchar('\n');
@@ -54,15 +44,15 @@ void create_numbers(int len, unsigned char letters, FILE*file,double file_size, 
 
 int main(int argc, char**argv)
 {
-	if (argc > 1 && (argv[1][0] == '?' || argv[1][1] == '?'))
+	if(argc!=4)
 	{
-		printf("pass_gen number_length base output_path\n");
+		puts("For usage and more information, check https://github.com/SKC-Developer/Passwords-Generator");
+		return 0;
 	}
 
-	if (argc < 4)return 0;
-
 	int len = atoi(argv[1]);
-	char letters = atoi(argv[2]);
+	char*letters = argv[2];
+	size_t base=strlen(letters);
 	FILE*file=fopen(argv[3], "w");
 	if (!file)
 	{
@@ -73,8 +63,8 @@ int main(int argc, char**argv)
 	printf("Printing ");
 	
 	//print number of numbers
-	u128 exp=p(letters, len);
 	int max = 0;
+	u128 exp=p(base,len);
 	for (; p(10, max) <= exp; max++);
 	
 	for (int i = 0; i < max; i++)
@@ -83,7 +73,7 @@ int main(int argc, char**argv)
 		exp%=p(10,max-1-i);
 	}
 	
-	printf(" numbers in base %d to file %s.\nIt will take about %f GBs. Is that OK?\n", letters, argv[3], ((double)p(letters, len)*(len + 2)) / (1024 * 1024 * 1024));
+	printf(" numbers in base %d to file %s.\nIt will take about %f GBs. Is that OK?\n", letters, argv[3], ((double)p(base, len)*(len + 2)) / (1024 * 1024 * 1024));
 
 	if (getchar() != 'y')
 	{
@@ -95,7 +85,7 @@ int main(int argc, char**argv)
 	printf("Would you like to see the progress?\n");
 	
 
-	create_numbers(len, letters, file,((double)p(letters, len)*(len + 2)) / (1024 * 1024 * 1024),getchar()=='y');
+	create_numbers(len, letters, file,((double)p(base, len)*(len + 2)) / (1024 * 1024 * 1024),getchar()=='y');
 
 	fclose(file);
 	return 0;
